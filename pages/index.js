e1a333a68f4bf4d4c74a0f740b165bd0bdd2ada8
Nep T. Orshiso <nep.orshiso@gmail.com>
2019-12-01 12:37:33 -0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import houses from './houses.json'
+
+const Index = () => (
+    <div>
+        <h2>Places to stay</h2>
+
+        <div className='houses'>
+            {houses.map((house, index) => {
+                
+            })}
+        </div>
+    </div>
+);
+
+export default Index;
